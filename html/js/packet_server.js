53944b1e40a75c26475b0e441ca3103c3f10e68b
Joshua Koo <zz85nus@gmail.com>
2019-07-27 06:47:20 -0700

    Include circular buffer as common runtime

--- html/js/packet_server.js
+++ html/js/packet_server.js
@@ -1,3 +1,30 @@
+class CircularBuffer {
+    constructor(size) {
+        size = size || 100;
+        this.buffer = new Array(size);
+        this.size = size;
+        this.pointer = 0;
+    }
+
+    put(item) {
+        var p = this.pointer;
+        this.buffer[p] = item;
+        this.pointer = (p + 1) % this.size;
+    }
+
+    last10() {
+        var p = this.pointer + this.size - 10;
+        var items = []
+        for (var i = 0; i < 10; i++) {
+            items.push(this.buffer[(i + p) % this.size]);
+        }
+
+        return items;
+    }
+}
+
+var buffer = new CircularBuffer();
+
 // connects to rust websockets server
 // and emits events
 function connect_packet_server(handler) {
@@ -6,6 +33,7 @@ function connect_packet_server(handler) {
         try {
             var data = JSON.parse(m.data);
             handler(data);
+            buffer.put(data);
         } catch (e) {
             console.log('error', m);
         }
@@ -13,4 +41,3 @@ function connect_packet_server(handler) {
     ws.onopen = () => console.log('connected');
     ws.onclosed = () => console.log('disconnected');
 }
-
