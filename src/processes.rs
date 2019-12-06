805e1224abd4dbe96dc80f666c589f10dd1ddb2b
Joshua Koo <zz85nus@gmail.com>
2019-12-06 01:36:43 -0800

    Comment difference between libproc vs sys crates

--- src/processes.rs
+++ src/processes.rs
@@ -28,6 +28,7 @@ pub fn netstats() {
     println!("used swap   : {} kB", sys.get_used_swap());
 
     /* unfortunately sys crate is a little buggy reading tcp6 sockets */
+    /* while for mac, sys uses c bindings to get memory, it's forking netstat for following */
     // netstat_mod(sys);
 
     /* use lower level libproc for mac sockets */
@@ -123,6 +124,7 @@ fn get_pid_path(pid: u32) -> Result<String, String> {
 
 fn processes_and_sockets() {
     if let Ok(pids) = proc_pid::listpids(ProcType::ProcAllPIDS) {
+        // TODO clean this up
         // pids
         //     .iter()
         //     .map(|pid| pidinfo::<BSDInfo>(pid as i32, 0))
@@ -293,41 +295,42 @@ fn convert_to_ipv6(addr: [u8; 16]) -> IpAddr {
     IpAddr::V6(Ipv6Addr::from(addr))
 }
 
+/* this can be used, but it crashes on IPv6 sockets */
 fn netstat_mod(sys: &mut System) {
-    // // ipv6 call crash
-    // let af_flags = AddressFamilyFlags::IPV4; // | AddressFamilyFlags::IPV6;
-    // let proto_flags = ProtocolFlags::TCP | ProtocolFlags::UDP;
-    // let sockets_info = get_sockets_info(af_flags, proto_flags).unwrap();
-    // for si in sockets_info {
-    //     // println!("{:?}",  si);
-    //     match si.protocol_socket_info {
-    //         ProtocolSocketInfo::Tcp(tcp_si) => {
-    //             println!(
-    //                 "TCP {}:{} -> {}:{} {:?} - {}",
-    //                 tcp_si.local_addr,
-    //                 tcp_si.local_port,
-    //                 tcp_si.remote_addr,
-    //                 tcp_si.remote_port,
-    //                 si.associated_pids,
-    //                 tcp_si.state
-    //             );
+    // ipv6 call crash
+    let af_flags = AddressFamilyFlags::IPV4; // | AddressFamilyFlags::IPV6;
+    let proto_flags = ProtocolFlags::TCP | ProtocolFlags::UDP;
+    let sockets_info = get_sockets_info(af_flags, proto_flags).unwrap();
+    for si in sockets_info {
+        // println!("{:?}",  si);
+        match si.protocol_socket_info {
+            ProtocolSocketInfo::Tcp(tcp_si) => {
+                println!(
+                    "TCP {}:{} -> {}:{} {:?} - {}",
+                    tcp_si.local_addr,
+                    tcp_si.local_port,
+                    tcp_si.remote_addr,
+                    tcp_si.remote_port,
+                    si.associated_pids,
+                    tcp_si.state
+                );
 
-    //             for pid in si.associated_pids {
-    //                 get_pid_info(&mut sys, pid);
-    //             }
-    //         }
-    //         ProtocolSocketInfo::Udp(udp_si) => {
-    //             println!(
-    //                 "UDP {}:{} -> *:* {:?}",
-    //                 udp_si.local_addr, udp_si.local_port, si.associated_pids
-    //             );
+                for pid in si.associated_pids {
+                    get_pid_info(&mut sys, pid);
+                }
+            }
+            ProtocolSocketInfo::Udp(udp_si) => {
+                println!(
+                    "UDP {}:{} -> *:* {:?}",
+                    udp_si.local_addr, udp_si.local_port, si.associated_pids
+                );
 
-    //             for pid in si.associated_pids {
-    //                 get_pid_info(&mut sys, pid);
-    //             }
-    //         }
-    //     }
-    // }
+                for pid in si.associated_pids {
+                    get_pid_info(&mut sys, pid);
+                }
+            }
+        }
+    }
 }
 
 fn get_pid_info(sys: &mut System, pid: u32) {
