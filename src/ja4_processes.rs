1fcfcfc840b1d84fcf7a3fc0bcd1bafe02c12b01
Joshua Koo <zz85nus@gmail.com>
2024-05-31 21:07:52 -0700

    ja4 processes binary - grouped by process name (refreshes 5 second)

--- src/ja4_processes.rs
+++ src/ja4_processes.rs
@@ -1,5 +1,16 @@
-use std::thread;
-use std::time::Duration;
+use crossterm::{
+    cursor,
+    style::{self, Stylize},
+    terminal, ExecutableCommand, QueueableCommand,
+};
+use std::{
+    collections::{HashMap, HashSet},
+    time::Duration,
+};
+use std::{
+    io::{self, Write},
+    thread,
+};
 
 use crossbeam::unbounded;
 use packet_radar::pcapng;
@@ -10,23 +21,45 @@ use tracing_subscriber;
 
 /// JA4 process collector
 /// Mac only, uses sudo to run with process mode
-fn main() {
+fn main() -> io::Result<()> {
     tracing_subscriber::fmt::init();
 
     let (tx, _rx) = unbounded::<PacketInfo>();
 
-    thread::spawn(|| loop {
-        println!("Processes");
+    thread::spawn(move || pcapng::pcap_parse("!", tx));
+
+    let mut stdout = io::stdout();
+
+    #[derive(Default, Debug)]
+    struct ProcInfo {
+        ja4: HashSet<String>,
+        pids: HashSet<u32>,
+    }
+
+    loop {
+        stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+        stdout
+            .queue(cursor::MoveTo(0, 0))?
+            .queue(style::PrintStyledContent(
+                "JA4 + Processes (refresh 5 seconds)\n".magenta(),
+            ))?;
+
+        let mut process_to_ja4 = HashMap::<_, ProcInfo>::new();
         for conn in TCP_STATS.conn_map.iter() {
             if let Some(ja4) = &conn.ja4 {
-                let pid = conn.pid.unwrap_or(0);
+                let pid = &conn.pid.unwrap_or(0);
                 let default = "".to_owned();
-                let process = conn.process_name.as_ref().unwrap_or_else(|| &default);
-                info!("{ja4} {process} {pid}");
+                let process = conn.process_name.clone().unwrap_or_else(|| default);
+                // info!("{ja4} {process} {pid}");
+
+                let entry = process_to_ja4.entry(process).or_default();
+                entry.ja4.insert(ja4.clone());
+                entry.pids.insert(*pid);
             }
         }
-        std::thread::sleep(Duration::from_millis(5000));
-    });
 
-    pcapng::pcap_parse("!", tx);
+        info!("{process_to_ja4:#?}");
+        stdout.flush()?;
+        std::thread::sleep(Duration::from_millis(5000));
+    }
 }
